361535507e675c503c46af10a8d8bb3ab9a905ae
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2023-11-10 11:16:54 +0100

    adding t function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{t("welcome_msg")}</p>
+        {/* <select>
+          <option onClick={t("mk")}>MK</option>
+          <option onClick={t("de")}>DE</option>
+          <option onClick={t("ar")}>AR</option>
+        </select> */}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
